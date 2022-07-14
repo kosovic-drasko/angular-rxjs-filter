efc4385f2205a2d0c46a4e84df0ac654eae255b1
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2022-07-14 22:32:53 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+   selector: 'app-root',
+   template: `
+		<app-filter-demo></app-filter-demo>
+    `
+})
+export class AppComponent { 
+}
+    
---
+++ src/app/filter-demo.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { MyCountry } from './my-country';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class FilterDemoService {
+    getNumbers(): Observable<number> {
+      return of(1, 2, 3, 4); //Synchronously emits 1, 2, 3, 4 and then completes
+    }
+    getStdNames(): Observable<string> {
+      return of("Mahesh, Krishna, Ram");
+    }	
+    getCountry(): Observable<MyCountry> {
+      return of(new MyCountry());
+    }
+}
